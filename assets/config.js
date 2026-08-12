322e6d5f4f7d44877f0f87596e40fcd057fa23b2
gazp20 <gazp20@icloud.com>
2026-08-12 14:11:45 +0100

    Update Google Form URL in config.js

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 window.FF_CONFIG = {
   // Paste your Google Form link here:
-  googleFormUrl: "https://docs.google.com/forms/d/e/1FAIpQLSc2mRgn_G4MqrJNe70UijjpvQaynzsepdcGi6laqrL5-ClKZQ/viewform",
+ googleFormUrl: "https://docs.google.com/forms/d/e/1FAIpQLSekz3ITHLuXPKM1x9CCAdIEgTfCZBq0VvCmdJqfejLXWi_oLQ/viewform?usp=header",
 
   // OPTIONAL: Publish Google Sheets tabs as CSV and paste the CSV URLs here.
   // Until you do, the website uses the sample JSON files in /data.
